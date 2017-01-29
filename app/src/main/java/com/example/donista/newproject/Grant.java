9d04171e85295a4f5a4ad5481ed9df99447fbadf
Donista <donista92@mail.ru>
2017-01-29 14:42:04 +0300

    Format lines

--- app/src/main/java/com/example/donista/newproject/Grant.java
+++ app/src/main/java/com/example/donista/newproject/Grant.java
@@ -4,22 +4,40 @@ package com.example.donista.newproject;
  * Created by Donista on 28.01.2017.
  */
 public class Grant {
-        private String grantName;
-        private  String grantDescription;
-        private  String grantDeadline;
-    public Grant(){}
+    private String grantName;
+    private String grantDescription;
+    private String grantDeadline;
 
-    public Grant(String grantName,String grantDescription,String grantDedline) {
-        this.grantName=grantName;
-        this.grantDescription=grantDescription;
-        this.grantDeadline=grantDedline;
+    public Grant() {
     }
-    public String getGrantName(){return grantName;}
-    public void setGrantName(String grantName){this.grantName=grantName;}
 
-    public String getGrantDescription() {return grantDescription;}
-    public void setGrantDescription(String grantDescription){this.grantDescription=grantDescription;}
+    public Grant(String grantName, String grantDescription, String grantDedline) {
+        this.grantName = grantName;
+        this.grantDescription = grantDescription;
+        this.grantDeadline = grantDedline;
+    }
 
-    public String getGrantDeadline(){return grantDeadline;}
-    public void  setGrantDeadline(String grantDedline){this.grantDeadline=grantDedline;}
+    public String getGrantName() {
+        return grantName;
+    }
+
+    public void setGrantName(String grantName) {
+        this.grantName = grantName;
+    }
+
+    public String getGrantDescription() {
+        return grantDescription;
+    }
+
+    public void setGrantDescription(String grantDescription) {
+        this.grantDescription = grantDescription;
+    }
+
+    public String getGrantDeadline() {
+        return grantDeadline;
+    }
+
+    public void setGrantDeadline(String grantDedline) {
+        this.grantDeadline = grantDedline;
+    }
 }
